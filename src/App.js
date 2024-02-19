e8f544622c5f09a36f3754b65c94b233bbeb0ad0
Pearlyvision <159009107+Pearlyvision@users.noreply.github.com>
2024-02-19 20:08:23 +0000

    Test 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ProgrammingList from './Components/programmingList'; // Assuming you have a component named ProgrammingList
+import ProgrammingParagraph from './Components/programmingParagraph'; // Assuming you have a component named ProgrammingParagraph
 
 function App() {
+  const [programming, setProgramming] = useState("String");
+
+  const chooseProgrammingLanguage = (concept) =>{
+    setProgramming(concept);    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+<header className="App-header">
+        <ProgrammingList concepts={programmingInfo} onSelect={chooseProgrammingLanguage} />
+        <ProgrammingParagraph concept={programming} /> 
       </header>
     </div>
   );
 }
 
 export default App;
+nfg-pfem-mtp
